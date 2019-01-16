2a76c452fe3e38604dd9f1e7f7749d10d43f9481
DenislavVelichkov <denisla.velichkov@gmail.com>
2019-01-17 01:02:15 +0200

    Project Update

---
+++ src/JavaAdvanced/StacksAndQueues_3/Excercise/BasicQueueOperations_4.java
@@ -0,0 +1,11 @@
+package JavaAdvanced.StacksAndQueues_3.Excercise;
+
+import java.util.Scanner;
+
+public class BasicQueueOperations_4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
---
+++ src/JavaAdvanced/StacksAndQueues_3/Excercise/MaximumElement_3.java
@@ -0,0 +1,11 @@
+package JavaAdvanced.StacksAndQueues_3.Excercise;
+
+import java.util.Scanner;
+
+public class MaximumElement_3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
