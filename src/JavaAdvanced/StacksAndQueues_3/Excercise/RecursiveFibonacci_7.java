f066793b1ed3e46bf6a631e2812d8a3625d3639f
Denzo <denislav.velichkov@gmail.com>
2019-01-16 20:28:15 +0200

    Project Update

--- src/JavaAdvanced/StacksAndQueues_3/Excercise/RecursiveFibonacci_7.java
+++ src/JavaAdvanced/StacksAndQueues_3/Excercise/RecursiveFibonacci_7.java
@@ -14,8 +14,9 @@ public class RecursiveFibonacci_7 {
     }
 
     private static long fibonacci(int n, long[] fibonacci) {
-        if (n == 0 || n == 1)
+        if (n == 0 || n == 1) {
             return 1;
+        }
         if (fibonacci[n] != 0) {
             return fibonacci[n];
         }
