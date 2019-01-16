59c1b053bc1c90e04ba6fc73c1f17ea880fc97dd
DenislavVelichkov <denisla.velichkov@gmail.com>
2019-01-17 00:56:51 +0200

    Project Update

---
+++ src/JavaAdvanced/StacksAndQueues_3/Excercise/BasicStackOperations_2.java
@@ -0,0 +1,57 @@
+package JavaAdvanced.StacksAndQueues_3.Excercise;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BasicStackOperations_2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] command = sc.nextLine().split("\\s+");
+        int pushCount = Integer.parseInt(command[0]);
+        int popCount = Integer.parseInt(command[1]);
+        int elementIsPresent = Integer.parseInt(command[2]);
+        ArrayDeque<Integer> elementsList = new ArrayDeque<>();
+        String[] elementsLine = sc.nextLine().split("\\s+");
+        int counter = 0;
+
+        while (pushCount != 0) {
+            elementsList.push((Integer.parseInt(elementsLine[counter])));
+            counter++;
+            pushCount--;
+        }
+
+        while (popCount != 0) {
+            elementsList.pop();
+            popCount--;
+        }
+
+        if (elementsList.contains(elementIsPresent)) {
+            System.out.println("true");
+        } else {
+            if (!elementsList.isEmpty()) {
+                int smallestEl = checkForSmallestEl(elementsList);
+                System.out.println(smallestEl);
+            } else {
+                System.out.println(elementsList.size());
+            }
+        }
+
+
+    }
+
+    private static int checkForSmallestEl(ArrayDeque<Integer> elementsList) {
+        int[] smallestNum = {elementsList.getFirst()};
+        final int[] result = {0};
+
+        elementsList.forEach(el -> {
+            if (smallestNum[0] <= el) {
+                result[0] = smallestNum[0];
+            } else {
+                smallestNum[0] = el;
+            }
+        });
+        return result[0];
+    }
+}
--- src/JavaAdvanced/StacksAndQueues_3/Excercise/ReverseNumbersWithAStack_1.java
+++ src/JavaAdvanced/StacksAndQueues_3/Excercise/ReverseNumbersWithAStack_1.java
@@ -1,5 +1,7 @@
 package JavaAdvanced.StacksAndQueues_3.Excercise;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReverseNumbersWithAStack_1 {
@@ -7,5 +9,16 @@ public class ReverseNumbersWithAStack_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String[] numbersInput = sc.nextLine().split("\\s+");
+        ArrayDeque<Integer> numbers = new ArrayDeque<>();
+
+        for (String number : numbersInput) {
+            int num = Integer.parseInt(number);
+            numbers.push(num);
+        }
+
+        while (numbers.size() != 0) {
+            System.out.print(numbers.pop() + " ");
+        }
     }
 }
