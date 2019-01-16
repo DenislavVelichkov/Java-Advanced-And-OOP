803878989ca3809c0bc6ea84f9ba777a27cba272
Denzo <denislav.velichkov@gmail.com>
2019-01-16 16:37:12 +0200

    Project Update

--- src/JavaAdvanced/StacksAndQueues_3/Excercise/ReverseNumbersWithAStack_1.java
+++ src/JavaAdvanced/StacksAndQueues_3/Excercise/ReverseNumbersWithAStack_1.java
@@ -1,4 +1,11 @@
 package JavaAdvanced.StacksAndQueues_3.Excercise;
 
+import java.util.Scanner;
+
 public class ReverseNumbersWithAStack_1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
 }
