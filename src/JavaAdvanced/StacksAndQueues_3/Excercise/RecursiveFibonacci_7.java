6a4ed9ceb6bb05363e99b63dfa113fee7367bb3a
Denzo <denislav.velichkov@gmail.com>
2019-01-17 17:15:38 +0200

    Project Update

--- src/JavaAdvanced/StacksAndQueues_3/Excercise/RecursiveFibonacci_7.java
+++ src/JavaAdvanced/StacksAndQueues_3/Excercise/RecursiveFibonacci_7.java
@@ -7,24 +7,24 @@ public class RecursiveFibonacci_7 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
-        long[] fibList = new long[n + 1];
+        long[] fibMemory = new long[n + 1];
 
-        long fibResult = calculateFibonacci(n, fibList);
+        long fibResult = calculateFibonacci(n, fibMemory);
         System.out.println(fibResult);
     }
 
-    private static long calculateFibonacci(int n, long[] fibonacci) {
+    private static long calculateFibonacci(int n, long[] memmory) {
         if (n == 0 || n == 1) {
             return 1;
         }
-        if (fibonacci[n] != 0) { //memoization
-            return fibonacci[n];
+        if (memmory[n] != 0) { //memoization
+            return memmory[n];
         }
 
-        long prev = calculateFibonacci(n - 1, fibonacci);
-        long prevPrev = calculateFibonacci(n - 2, fibonacci);
+        long prev = calculateFibonacci(n - 1, memmory);
+        long prevPrev = calculateFibonacci(n - 2, memmory);
         long result = prev + prevPrev;
-        fibonacci[n] = result;
+        memmory[n] = result;
         return result;
     }
 }
