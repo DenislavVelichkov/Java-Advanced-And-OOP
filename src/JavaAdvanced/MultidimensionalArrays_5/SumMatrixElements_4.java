5ee08fc5d8edc90c9fd7822bb338757848face93
Denzo <denislav.velichkov@gmail.com>
2019-01-19 15:12:58 +0200

    Project Update

--- src/JavaAdvanced/MultidimensionalArrays_5/SumMatrixElements_4.java
+++ src/JavaAdvanced/MultidimensionalArrays_5/SumMatrixElements_4.java
@@ -1,5 +1,6 @@
 package JavaAdvanced.MultidimensionalArrays_5;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SumMatrixElements_4 {
@@ -7,5 +8,20 @@ public class SumMatrixElements_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int[] dimensions = Arrays.stream(sc.nextLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = 0;
+        for (int i = 0; i < dimensions[0]; i++) {
+            sum += Arrays.stream(sc.nextLine().split(", "))
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+
+        }
+
+        System.out.println(dimensions[0]);
+        System.out.println(dimensions[1]);
+        System.out.println(sum);
     }
 }
