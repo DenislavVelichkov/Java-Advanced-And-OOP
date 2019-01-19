e871cdc23b7924cfca314386958d4ec1c8db05f8
Denzo <denislav.velichkov@gmail.com>
2019-01-19 17:22:17 +0200

    Project Update

--- src/JavaAdvanced/MultidimensionalArrays_5/MaximumSumOf2x2Submatrix_5.java
+++ src/JavaAdvanced/MultidimensionalArrays_5/MaximumSumOf2x2Submatrix_5.java
@@ -1,5 +1,6 @@
 package JavaAdvanced.MultidimensionalArrays_5;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MaximumSumOf2x2Submatrix_5 {
@@ -7,5 +8,53 @@ public class MaximumSumOf2x2Submatrix_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int[] size = Arrays.stream(sc.nextLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int[][] matrix = new int[size[0]][size[1]];
+
+        for (int i = 0; i < size[0]; i++) {
+            int[] fillRows = Arrays.stream(sc.nextLine().split(", "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            matrix[i] = fillRows;
+        }
+
+        int maxValue = Integer.MIN_VALUE;
+        int matrixSum = -1;
+        int maxRow = 0;
+        int maxCol = 0;
+
+        for (int row = 0; row < size[0] - 1; row++) {
+            for (int col = 0; col < size[1] - 1; col++) {
+                matrixSum = calcMatrixSum(matrix, row, col);
+                if (matrixSum == 0) continue;
+                if (matrixSum >= maxValue) {
+                    maxValue = matrixSum;
+                    maxRow = row;
+                    maxCol = col;
+                }
+            }
+        }
+
+        System.out.printf("%d %d\n%d %d\n", matrix[maxRow][maxCol],
+                matrix[maxRow][maxCol + 1],
+                matrix[maxRow + 1][maxCol],
+                matrix[maxRow + 1][maxCol + 1],
+                matrixSum);
+
+    }
+
+    private static int calcMatrixSum(int[][] matrix, int row, int col) {
+        if (row == matrix.length || col == matrix.length) {
+            return 0;
+        } else {
+            return matrix[row][col] +
+                    matrix[row][col + 1] +
+                    matrix[row + 1][col] +
+                    matrix[row + 1][col + 1];
+        }
     }
 }
+
