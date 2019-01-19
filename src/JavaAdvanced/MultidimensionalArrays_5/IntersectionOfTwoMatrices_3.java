33944fbd871e57f11cf242e9307095fcd83068d8
Denzo <denislav.velichkov@gmail.com>
2019-01-19 15:03:06 +0200

    Project Update

--- src/JavaAdvanced/MultidimensionalArrays_5/IntersectionOfTwoMatrices_3.java
+++ src/JavaAdvanced/MultidimensionalArrays_5/IntersectionOfTwoMatrices_3.java
@@ -7,5 +7,36 @@ public class IntersectionOfTwoMatrices_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int rows = Integer.parseInt(sc.nextLine());
+        int columns = Integer.parseInt(sc.nextLine());
+
+        char[][] matrixA = new char[rows][columns];
+        char[][] matrixB = new char[rows][columns];
+
+
+        for (int i = 0; i < rows; i++) {
+            String[] fillRows = sc.nextLine().split("\\s+");
+            for (int j = 0; j < columns; j++) {
+                matrixA[i][j] = fillRows[j].charAt(0);
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            String[] fillRows = sc.nextLine().split("\\s+");
+            for (int j = 0; j < columns; j++) {
+                matrixB[i][j] = fillRows[j].charAt(0);
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrixA[i][j] == matrixB[i][j]) {
+                    System.out.print(matrixA[i][j] + " ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
