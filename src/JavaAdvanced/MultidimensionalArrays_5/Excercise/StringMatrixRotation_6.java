6d5e6fdb92ba6e08673a711145905852306a8787
Denzo <denislav.velichkov@gmail.com>
2019-01-22 00:57:52 +0200

    Project Update

--- src/JavaAdvanced/MultidimensionalArrays_5/Excercise/StringMatrixRotation_6.java
+++ src/JavaAdvanced/MultidimensionalArrays_5/Excercise/StringMatrixRotation_6.java
@@ -11,19 +11,19 @@ public class StringMatrixRotation_6 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         String[] instructions = reader.readLine().split("[()]");
-        int degrees = Integer.parseInt(instructions[1]);
+        int degrees = Integer.parseInt(instructions[1]) % 360;
         String input = reader.readLine();
-        ArrayList<String> rows = new ArrayList<>();
+        ArrayList<String> inputTxt = new ArrayList<>();
 
         while (!input.equals("END")) {
-            rows.add(input);
+            inputTxt.add(input);
             input = reader.readLine();
         }
 
         String[] longestElement = {""};
         int[] maxLength = {Integer.MIN_VALUE};
 
-        rows
+        inputTxt
                 .forEach(s -> {
 
                     if (s.length() > maxLength[0]) {
@@ -32,18 +32,58 @@ public class StringMatrixRotation_6 {
                     }
                 });
 
-        int rowsSize = rows.size();
+        int rowsSize = inputTxt.size();
         int columnSize = longestElement[0].length();
 
-        String[][] matrix = new String[rowsSize][columnSize];
-        for (int i = 0; i < matrix.length; i++) {
-            String row = fixRows(rows.get(i).toCharArray(), columnSize);
-            for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = String.valueOf(row.charAt(j));
+        char[][] matrix = new char[rowsSize][columnSize];
+
+        for (int r = 0; r < inputTxt.size(); r++) {
+            for (int c = 0; c < maxLength[0]; c++) {
+                if (c > inputTxt.get(r).length() - 1) {
+                    matrix[r][c] = ' ';
+                } else {
+                    matrix[r][c] = inputTxt.get(r).charAt(c);
+                }
             }
         }
 
-        int sizeX;
+        switch (degrees) {
+            case 90:
+                for (int c = 0; c < maxLength[0]; c++) {
+                    for (int r = matrix.length - 1; r >= 0; r--) {
+                        System.out.print(matrix[r][c]);
+                    }
+                    System.out.println();
+                }
+                break;
+            case 180:
+                for (int r = matrix.length - 1; r >= 0; r--) {
+                    for (int c = maxLength[0] - 1; c >= 0; c--) {
+                        System.out.print(matrix[r][c]);
+                    }
+                    System.out.println();
+                }
+                break;
+            case 270:
+                for (int c = maxLength[0] - 1; c >= 0; c--) {
+                    for (int r = 0; r < matrix.length; r++) {
+                        System.out.print(matrix[r][c]);
+                    }
+                    System.out.println();
+                }
+                break;
+            default:
+                for (int r = 0; r < matrix.length; r++) {
+                    for (int c = 0; c < matrix[r].length; c++) {
+                        System.out.print(matrix[r][c]);
+                    }
+                    System.out.println();
+                }
+                break;
+        }
+    }
+}
+/*        int sizeX;
         int sizeY;
         switch (degrees) {
             case 90:
@@ -83,11 +123,7 @@ public class StringMatrixRotation_6 {
     }
 
     private static String[][] rotateMatrix90(String[][] matrix, int sizeX, int sizeY) {
-        String[][] tempMatrix = new String[sizeX][sizeY];
-        int rows = (int) Math.ceil(sizeX / 2);
-        for (int i = 0; i < Math.ceil(sizeX / 2); i++) {
-            
-        }
+
       return matrix;
     }
 
@@ -98,5 +134,5 @@ public class StringMatrixRotation_6 {
         chars.append(arr).append(stringToRepeat.repeat(remainingLengthToFill));
         return chars.toString();
     }
-}
+}*/
 
