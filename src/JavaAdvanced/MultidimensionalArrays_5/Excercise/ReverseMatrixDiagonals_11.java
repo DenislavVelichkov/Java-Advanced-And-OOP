ea7072b2b8a555a11dd35fe7e9d4826222e4e886
Denzo <denislav.velichkov@gmail.com>
2019-01-21 23:55:39 +0200

    Project Update

--- src/JavaAdvanced/MultidimensionalArrays_5/Excercise/ReverseMatrixDiagonals_11.java
+++ src/JavaAdvanced/MultidimensionalArrays_5/Excercise/ReverseMatrixDiagonals_11.java
@@ -1,5 +1,6 @@
 package JavaAdvanced.MultidimensionalArrays_5.Excercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReverseMatrixDiagonals_11 {
@@ -7,5 +8,25 @@ public class ReverseMatrixDiagonals_11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String[] input = sc.nextLine().split("\\s+");
+
+        int rows = Integer.parseInt(input[0]);
+        int cols = Integer.parseInt(input[1]);
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int row = 0; row < rows; i++) {
+            matrix[row] = Arrays.stream(sc.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+
+        int row = rows - 1;
+        int col = cols - 1;
+
+        while (row != -1) {
+
+
+        }
     }
 }
