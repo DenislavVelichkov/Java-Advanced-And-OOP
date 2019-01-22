43e4c5b6433fc5796bd0cd9dd27fca6e4e7b9095
DenislavVelichkov <denisla.velichkov@gmail.com>
2019-01-22 22:06:23 +0200

    Project Update

--- src/JavaAdvanced/SetsAndMaps_7/AcademyGraduation_8.java
+++ src/JavaAdvanced/SetsAndMaps_7/AcademyGraduation_8.java
@@ -1,11 +1,39 @@
 package JavaAdvanced.SetsAndMaps_7;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class AcademyGraduation_8 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int studentsCount = Integer.parseInt(sc.nextLine());
+        Map<String, ArrayList<Double>> listGrades = new LinkedHashMap<>();
+
+        for (int i = 0; i < studentsCount; i++) {
+            String studentName = sc.nextLine();
+            double[] grades = Arrays.stream(sc.nextLine().split("\\s+"))
+                    .mapToDouble(Double::parseDouble)
+                    .toArray();
+
+            listGrades.putIfAbsent(studentName, new ArrayList<>());
+            for (double grade : grades) {
+                listGrades.get(studentName).add(grade);
+            }
+        }
+
+        listGrades
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparing(Map.Entry::getKey))
+                .forEach(entry -> {
+                    Double sum =
+                            entry
+                                    .getValue()
+                                    .stream()
+                                    .mapToDouble(value -> value).sum();
+                    System.out.printf("%s is graduated with %f\n",
+                            entry.getKey(), sum / entry.getValue().size());
+                });
 
     }
 }
