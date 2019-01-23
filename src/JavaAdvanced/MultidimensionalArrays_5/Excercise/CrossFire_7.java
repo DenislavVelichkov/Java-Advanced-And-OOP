1d8c48529929ed00617417b801304226e9206cc7
Denzo <denislav.velichkov@gmail.com>
2019-01-23 21:05:22 +0200

    Project Update

--- src/JavaAdvanced/MultidimensionalArrays_5/Excercise/CrossFire_7.java
+++ src/JavaAdvanced/MultidimensionalArrays_5/Excercise/CrossFire_7.java
@@ -1,29 +1,36 @@
 package JavaAdvanced.MultidimensionalArrays_5.Excercise;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class CrossFire_7 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] dimensions = Arrays.stream(sc.nextLine().split("\\s+"))
+
+        int[] dimensions = Arrays.stream(reader.readLine().split("\\s+"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
-
-        Map<Integer, ArrayList<Integer>> rows = new LinkedHashMap<>();
+        Map<Integer, ArrayList<String>> rows = new LinkedHashMap<>();
         int element = 1;
 
-        for (int i = 0; i < dimensions[0] * dimensions[1]; i++) {
+        for (int i = 0; i < dimensions[0]; i++) {
             rows.putIfAbsent(i, new ArrayList<>());
-            rows.get(i).add(element++);
+            while (rows.get(i).size() < dimensions[0]) {
+                String toAdd = "" + element++;
+                rows.get(i).add(toAdd);
+            }
         }
 
-
-        String input = sc.nextLine();
+        String input = reader.readLine();
+        Map<Integer, ArrayList<String>> rowsAfterCrossfire = new LinkedHashMap<>();
 
         while (!input.equals("Nuke it from orbit")) {
-            int[]coordinates = Arrays.stream(input.split("\\s+"))
+            int[] coordinates = Arrays.stream(input.split("\\s+"))
                     .mapToInt(Integer::parseInt)
                     .toArray();
 
@@ -31,22 +38,61 @@ public class CrossFire_7 {
             int col = coordinates[1];
             int radius = coordinates[2];
 
-            printMatrix(rows, row, col, radius, dimensions[0], dimensions[1]);
-            input = sc.nextLine();
+            rowsAfterCrossfire = crossfire(rows, row, col, radius);
+
+
+            input = reader.readLine();
         }
 
+        StringBuilder matrix = new StringBuilder();
 
+        for (int i = 0; i < rowsAfterCrossfire.size(); i++) {
+            for (int j = 0; j < rowsAfterCrossfire.get(i).size(); j++) {
+                matrix.append(rowsAfterCrossfire.get(i).get(j)).append(" ");
+            }
+            System.out.println(matrix);
+            matrix = new StringBuilder();
+        }
     }
 
-    private static void printMatrix(Map<Integer, ArrayList<Integer>> rows,
-                                    int row,
-                                    int col,
-                                    int radius,
-                                    int x,
-                                    int y) {
-        int elementIndex = 0;
+    private static Map<Integer, ArrayList<String>>
+        crossfire(Map<Integer, ArrayList<String>> rows,
+                                                int row,
+                                                int col,
+                                                int radius) {
+        Deque<String> queue = new ArrayDeque<>();
+        queue.push(rows.get(row).get(col));
 
+        while (radius > 0) {
+            try {
+                queue.push(rows.get(row - radius).get(col));
+                rows.get(row - radius).remove(col);
+            } catch (Exception ignored) {
+            }
 
+            try {
+                queue.push(rows.get(row + radius).get(col));
+                rows.get(row + radius).remove(col);
+            } catch (Exception ignored) {
+            }
 
+            try {
+                queue.push(rows.get(row).get(col + radius));
+            } catch (Exception ignored) {
+            }
+
+            try {
+                queue.push(rows.get(row).get(col - radius));
+            } catch (Exception ignored) {
+            }
+
+            radius--;
+        }
+
+        while (queue.size() != 0) {
+            rows.get(row).remove(queue.pop());
+        }
+
+        return rows;
     }
 }
