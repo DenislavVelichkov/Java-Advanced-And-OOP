840178ed07b9329eb69a916f85a92b0bdf976030
Denzo <denislav.velichkov@gmail.com>
2019-01-24 15:56:31 +0200

    Project Update

--- src/JavaAdvanced/SetsAndMaps_7/Excercise/HandsOfCards_8.java
+++ src/JavaAdvanced/SetsAndMaps_7/Excercise/HandsOfCards_8.java
@@ -1,13 +1,31 @@
 package JavaAdvanced.SetsAndMaps_7.Excercise;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class HandsOfCards_8 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String input = sc.nextLine();
+        HashMap<String, Set<String>> playersDecks = new LinkedHashMap<>();
 
+        while (!input.equals("JOKER")) {
+            String[] tokens = input.split(": ");
+
+            String name = tokens[0];
+            Set<String> cards = Arrays.stream(tokens[1].split(", +"))
+                    .collect(Collectors.toCollection(HashSet::new));
+
+            if (!playersDecks.containsKey(name)) {
+                playersDecks.put(name, cards);
+            } else {
+                playersDecks.get(name).addAll(cards);
+            }
+
+            input = sc.nextLine();
+        }
 
     }
 }
