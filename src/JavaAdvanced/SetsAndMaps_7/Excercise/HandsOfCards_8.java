c4b5bf2c66088379518e382b9e5a9cf5f6acf82b
Denzo <denislav.velichkov@gmail.com>
2019-01-24 15:49:00 +0200

    Project Update

---
+++ src/JavaAdvanced/SetsAndMaps_7/Excercise/HandsOfCards_8.java
@@ -0,0 +1,13 @@
+package JavaAdvanced.SetsAndMaps_7.Excercise;
+
+import java.util.Scanner;
+
+public class HandsOfCards_8 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+    }
+}
