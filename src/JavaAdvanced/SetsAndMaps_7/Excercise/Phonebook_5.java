82a5913bbaf6249a1ed6ddcae28c673599f21b34
Denzo <denislav.velichkov@gmail.com>
2019-01-25 16:27:34 +0200

    Project Update

---
+++ src/JavaAdvanced/SetsAndMaps_7/Excercise/Phonebook_5.java
@@ -0,0 +1,13 @@
+package JavaAdvanced.SetsAndMaps_7.Excercise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Phonebook_5 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
