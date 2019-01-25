71ed6d2968bb185c89fa4f37b041f8bd89cda7cb
DenislavVelichkov <denisla.velichkov@gmail.com>
2019-01-25 20:11:57 +0200

    Project Update

--- src/JavaAdvanced/StreamsFilesAndDirectories_9/demo.java
+++ src/JavaAdvanced/StreamsFilesAndDirectories_9/demo.java
@@ -1,5 +1,37 @@
 package JavaAdvanced.StreamsFilesAndDirectories_9;
 
-public class demo {
+import java.io.*;
 
+public class demo {
+    public static void main(String[] args) throws IOException {
+        String basePath = "";
+        String inputPath = "";
+        String outputPath = "";
+
+
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+        int row = 1;
+
+        try {
+            reader = new BufferedReader(new FileReader(inputPath));
+            writer = new PrintWriter(new FileWriter(outputPath));
+            String line = reader.readLine();
+
+            while (line != null) {
+                if (row % 3 == 0) {
+                    writer.println(line);
+                }
+
+                row++;
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+    }
 }
