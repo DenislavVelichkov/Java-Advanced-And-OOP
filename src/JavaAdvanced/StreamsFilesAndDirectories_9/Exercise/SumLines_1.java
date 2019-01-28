51dd6c1db218dfb8151ffb1f9d8f94e43c87fa75
Denzo <denislav.velichkov@gmail.com>
2019-01-28 21:44:40 +0200

    Project Update

---
+++ src/JavaAdvanced/StreamsFilesAndDirectories_9/Exercise/SumLines_1.java
@@ -0,0 +1,11 @@
+package JavaAdvanced.StreamsFilesAndDirectories_9.Exercise;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SumLines_1 {
+
+    public static void main(String[] args) throws IOException {
+        FileReader fileReader = new FileReader("D:\\SoftUni\\Advanced Modules\\Java Fundamentals\\Java Advanced Source Code\\src\\JavaAdvanced\\StreamsFilesAndDirectories_9\\Exercise");
+    }
+}
