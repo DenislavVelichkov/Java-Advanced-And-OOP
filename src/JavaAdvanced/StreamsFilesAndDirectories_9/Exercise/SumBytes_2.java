9e5fc828b4d8472555baa05a6d4d5cc40e505980
Denzo <denislav.velichkov@gmail.com>
2019-02-03 20:32:06 +0200

    Project Update

--- src/JavaAdvanced/StreamsFilesAndDirectories_9/Exercise/SumBytes_2.java
+++ src/JavaAdvanced/StreamsFilesAndDirectories_9/Exercise/SumBytes_2.java
@@ -1,13 +1,35 @@
 package JavaAdvanced.StreamsFilesAndDirectories_9.Exercise;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class SumBytes_2 {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream inputStream = new FileInputStream(
+                "D:\\SoftUni\\Advanced Modules\\Java Fundamentals\\" +
+                        "Java Advanced Source Code\\src\\JavaAdvanced\\" +
+                        "StreamsFilesAndDirectories_9\\Exercise\\input.txt");
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream)); // read file
+
+        String line = reader.readLine();
+        int totalSum = 0;
+
+        while (line != null) {
+            int sum = 0;
+
+            for (char c : line.toCharArray()) {
+                sum += c;
+            }
+
+            totalSum += sum;
+
+            line = reader.readLine();
+        }
+
+        System.out.println(totalSum);
     }
 }
