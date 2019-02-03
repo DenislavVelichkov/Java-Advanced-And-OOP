da78e9c00f668ccd7d7448fdf71d204bc677d611
Denzo <denislav.velichkov@gmail.com>
2019-02-03 21:20:37 +0200

    Project Update

--- src/JavaAdvanced/StreamsFilesAndDirectories_9/Exercise/CopyAPicture_9.java
+++ src/JavaAdvanced/StreamsFilesAndDirectories_9/Exercise/CopyAPicture_9.java
@@ -5,10 +5,12 @@ import java.io.*;
 public class CopyAPicture_9 {
 
     public static void main(String[] args) throws IOException {
-        FileInputStream inputStream = new FileInputStream("java.jpg");
+        FileInputStream inputStream = new FileInputStream("src\\JavaAdvanced\\" +
+                "StreamsFilesAndDirectories_9\\Exercise\\Roarin Lion.jpg"); //get picture Bytes
+
         byte[] buffer = inputStream.readAllBytes();
 
-        FileOutputStream outputStream = new FileOutputStream("copy-java.jpg");
+        FileOutputStream outputStream = new FileOutputStream("copy-RoaringLion.jpg");
 
         outputStream.write(buffer);
         outputStream.close();
