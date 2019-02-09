e57b08652b349f08e3e4daef564e3d8f3ca523c2
Denzo <denislav.velichkov@gmail.com>
2019-02-09 23:21:41 +0200

    Project Update

--- src/JavaAdvanced/Generics_17/ListUtilities_4/Main.java
+++ src/JavaAdvanced/Generics_17/ListUtilities_4/Main.java
@@ -1,7 +1,18 @@
 package JavaAdvanced.Generics_17.ListUtilities_4;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> asd = new ArrayList<>();
+        Collections.addAll(asd, 1, 2, 18, 2 - 1);
+        ListUtils.getMin(asd);
+
+        List<String> dsa = new ArrayList<>();
+        Collections.addAll(dsa, "aka", "bala", "mica");
+        ListUtils.getMax(dsa);
 
     }
 }
