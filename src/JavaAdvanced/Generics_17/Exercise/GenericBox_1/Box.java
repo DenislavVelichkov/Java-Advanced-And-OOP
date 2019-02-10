5f4b6516d7473314dfca0582fc6130465ed1ad5e
Denzo <denislav.velichkov@gmail.com>
2019-02-10 13:06:21 +0200

    Project Update

--- src/JavaAdvanced/Generics_17/Exercise/GenericBox_1/Box.java
+++ src/JavaAdvanced/Generics_17/Exercise/GenericBox_1/Box.java
@@ -1,35 +1,24 @@
 package JavaAdvanced.Generics_17.Exercise.GenericBox_1;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Box<T> {
-    private List<T> params;
-    private List<Integer> intParams;
+    private ArrayList<T> params;
 
     Box() {
         this.params = new ArrayList<>();
     }
 
-    public void add(T param) {
-        if (param instanceof String) {
-            this.params.add(param);
-        }
-        if (param instanceof Integer) {
-            int num = (int) param;
-            this.intParams.add(num);
-        }
+    public void add(ArrayList<T> param) {
+        this.params.addAll(param);
     }
+
     @Override
     public String toString() {
         StringBuilder str = new StringBuilder();
-        this.params.forEach(param -> str
-                .append(param.getClass().getName())
-                .append(": ")
-                .append(param)
-                .append(System.lineSeparator()));
+        str.append(params.get(0).getClass().getName()).append(": ");
+        this.params.forEach(param -> str.append(param).append(" "));
 
         return str.toString();
     }
-
 }
--- src/JavaAdvanced/Generics_17/Exercise/GenericBox_1/Main.java
+++ src/JavaAdvanced/Generics_17/Exercise/GenericBox_1/Main.java
@@ -1,22 +1,32 @@
 package JavaAdvanced.Generics_17.Exercise.GenericBox_1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = Integer.parseInt(sc.nextLine());
 
-        String line = sc.nextLine();
-        Box box = new Box<>();
-
         while (n-- > 0) {
-            box.add(line);
+            String[] tokens = sc.nextLine().split("\\s+");
+            Box box = new Box<Integer>();
 
-            line = sc.nextLine();
+            try {
+                ArrayList temp = Arrays.stream(tokens).map(Integer::parseInt).collect(Collectors.toCollection(ArrayList::new));
+                box.add(temp);
+                System.out.println(box);
+            } catch (Exception e) {
+                box = new Box<String>();
+                ArrayList temp = Arrays.stream(tokens).collect(Collectors.toCollection(ArrayList::new));
+                box.add(temp);
+                System.out.println(box);
+            }
         }
-        System.out.println(box);
-    }
 
+    }
 }
