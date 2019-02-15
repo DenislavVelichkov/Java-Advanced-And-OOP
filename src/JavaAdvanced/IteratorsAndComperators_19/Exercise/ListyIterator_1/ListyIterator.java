c25d683e0a29aa1234b4fce61795f8c154b41d99
Denzo <denislav.velichkov@gmail.com>
2019-02-16 01:15:50 +0200

    Project Update

--- src/JavaAdvanced/IteratorsAndComperators_19/Exercise/ListyIterator_1/ListyIterator.java
+++ src/JavaAdvanced/IteratorsAndComperators_19/Exercise/ListyIterator_1/ListyIterator.java
@@ -1,13 +1,11 @@
 package JavaAdvanced.IteratorsAndComperators_19.Exercise.ListyIterator_1;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-public class ListyIterator implements Iterable<T> {
-    private static final int INDEX = -1;
+public class ListyIterator implements Iterable<String> {
+    private static final int INIT_INDEX = -1;
     private List<String> data;
     private int index;
 
@@ -30,22 +28,22 @@ public class ListyIterator implements Iterable<T> {
 
 
     @Override
-    public Iterator<> iterator() {
+    public Iterator<String> iterator() {
         return new Iterator<>() {
-            int index = 0;
+            int index = INIT_INDEX;
 
             @Override
             public boolean hasNext() {
-                return index < collection.size();
+                return index < data.size();
             }
 
             @Override
-            public T next() {
-                if (collection.isEmpty()) {
+            public String next() {
+                if (data.isEmpty()) {
                     System.out.println("Invalid Operation!");
                 }
 
-                return collection.get(index++);
+                return data.get(index++);
             }
         };
     }
