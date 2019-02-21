459e64e9e7fa5d185bf2527f4cdf7ddf916dc11f
DenislavVelichkov <denisla.velichkov@gmail.com>
2019-02-22 01:06:40 +0200

    Project Update

--- src/JavaAdvanced/Workshop_Algorithms_21/Exercise/ConnectedAreasInAMatrix_3.java
+++ src/JavaAdvanced/Workshop_Algorithms_21/Exercise/ConnectedAreasInAMatrix_3.java
@@ -1,12 +1,106 @@
 package JavaAdvanced.Workshop_Algorithms_21.Exercise;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ConnectedAreasInAMatrix_3 {
+
+   public static class Area {
+        private int row;
+        private int col;
+        private int size;
+
+        Area(int r, int c, int size) {
+            this.row = r;
+            this.col = c;
+            this.size = size;
+        }
+
+        public int getRow() {
+            return this.row;
+        }
+
+        public int getCol() {
+            return this.col;
+        }
+
+        public int getSize() {
+            return this.size;
+        }
+
+        public String toString(int number) {
+            return String.format("Area #%d at (%d, %d), size: %d", number, this.row, this.col, this.size);
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int rows = Integer.parseInt(sc.nextLine());
+        int cols = Integer.parseInt(sc.nextLine());
 
+        char[][] matrix = new char[rows][cols];
+        ArrayList<Area> areas = new ArrayList<>();
+
+        for (int r = 0; r < matrix.length; r++) {
+            matrix[r] = sc.nextLine().replaceAll("[\t ]+", "").toCharArray();
+        }
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+
+                if (matrix[r][c] == '-') {
+                    int[] size = new int[1];
+                    countSize(matrix, r, c, size);
+                    Area area = new Area(r, c, size[0]);
+                    areas.add(area);
+                }
+
+            }
+        }
+
+        System.out.println("Total areas found: " + areas.size());
+        AtomicInteger number = new AtomicInteger(1);
+
+        areas.stream().sorted((f, s) -> {
+            int result = s.getSize() - f.getSize();
+
+            if (result == 0) {
+                result = f.getRow() - s.getRow();
+            }
+            if (result == 0) {
+                result = f.getCol() - s.getCol();
+
+            }
+
+            return result;
+
+        }).forEach(area -> System.out.println(area.toString(number.getAndIncrement())));
+
+    }
+
+    private static void countSize(char[][] matrix, int r, int c, int[] size) {
+        if (!isInBounds(matrix, r, c) ||
+                        matrix[r][c] == 'V' ||
+                        matrix[r][c] == '*') {
+            return;
+        }
+
+        size[0]++;
+        matrix[r][c] = 'V';
+
+        countSize(matrix, r + 1, c, size);
+        countSize(matrix, r, c + 1, size);
+        countSize(matrix, r - 1, c, size);
+        countSize(matrix, r, c - 1, size);
+
+    }
+
+    private static boolean isInBounds(char[][] matrix, int r, int c) {
+        return r >= 0
+                && r < matrix.length
+                && c >= 0
+                && c < matrix[r].length;
     }
 }
