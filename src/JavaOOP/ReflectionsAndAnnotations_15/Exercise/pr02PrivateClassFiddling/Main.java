d83f5e3759fede6abfaeefa8c4d736613186344e
Denzo <denislav.velichkov@gmail.com>
2019-03-30 18:05:00 +0200

    Project Update

--- src/JavaOOP/ReflectionsAndAnnotations_15/Exercise/pr02PrivateClassFiddling/Main.java
+++ src/JavaOOP/ReflectionsAndAnnotations_15/Exercise/pr02PrivateClassFiddling/Main.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -14,27 +15,30 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String input = sc.nextLine();
-        BlackBoxInt blackBoxInt;
 
-        Constructor constructor = BlackBoxInt.class.getDeclaredConstructor();
+        Constructor<BlackBoxInt> constructor = BlackBoxInt.class.getDeclaredConstructor();
         constructor.setAccessible(true);
 
-        blackBoxInt = (BlackBoxInt) constructor.newInstance();
-        while (!input.equals("END")) {
+        BlackBoxInt blackBoxInt = (BlackBoxInt) constructor.newInstance();
+
+        Method[] methods = blackBoxInt.getClass().getDeclaredMethods();
+        Field field = blackBoxInt.getClass().getDeclaredField("innerValue");
+        field.setAccessible(true);
+
+        while (!input.equalsIgnoreCase("END")) {
+
             String[] tokens = input.split("_");
             String command = tokens[0];
             int number = Integer.parseInt(tokens[1]);
 
-
-            Method method = blackBoxInt.getClass()
-                                .getDeclaredMethod(command, int.class);
+            Method method = Arrays.stream(methods)
+                             .filter(m -> m.getName().equalsIgnoreCase(command))
+                             .findFirst().orElse(null);
 
             method.setAccessible(true);
             method.invoke(blackBoxInt, number);
 
-            Field field = blackBoxInt.getClass().getDeclaredField("innerValue");
 
-            field.setAccessible(true);
             System.out.println(field.get(blackBoxInt));
 
             input = sc.nextLine();
