f74e0dc7eb6f77bb88d06e405e76c568e99c3542
DenislavVelichkov <denislav.velichkov@gmail.com>
2019-08-28 08:54:57 +0300

    New Project structures added

---
+++ Java OOP Exercises/src/JavaAdvanced/Generics_17/Exercise/GenericSwapMethodString_3/Box.java
@@ -0,0 +1,36 @@
+package JavaAdvanced.Generics_17.Exercise.GenericSwapMethodString_3;
+
+import java.util.ArrayList;
+
+public class Box<T> {
+    private ArrayList<T> params;
+
+    Box() {
+        this.params = new ArrayList<>();
+    }
+
+    public void add(T param) {
+        this.params.add(param);
+    }
+
+    public void swap(int first, int second) {
+        T val1 = this.params.get(first);
+        T val2 = this.params.get(second);
+        this.params.set(first, val2);
+        this.params.set(second, val1);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+
+        this.params
+                .forEach(param -> str
+                .append(params.get(0).getClass().getName())
+                .append(": ")
+                .append(param)
+                .append(System.lineSeparator()));
+
+        return str.toString();
+    }
+}
